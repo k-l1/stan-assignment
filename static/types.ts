e6deda151547af8b5d01af44a37e615566b9c3fc
k-l1 <karl.l1@icloud.com>
2024-02-02 14:15:48 +1100

    feat: Add common program type

---
+++ static/types.ts
@@ -0,0 +1,29 @@
+export type Program = {
+  id: number;
+  title: string;
+  description: string;
+  type: Type;
+  image: string;
+  rating: Rating;
+  genre: string;
+  year: number;
+  language: Language;
+};
+
+export enum Language {
+  English = "English",
+  French = "French",
+  Iranian = "Iranian",
+  Spanish = "Spanish",
+}
+export enum Rating {
+  M = "M",
+  Ma15 = "MA 15+",
+  PG = "PG",
+  R18 = "R 18+",
+}
+
+export enum Type {
+  Movie = "movie",
+  Series = "series",
+}
