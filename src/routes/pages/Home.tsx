a4a76aa2e649fa88eeaeee442dbb2ececbbff116
k-l1 <karl.l1@icloud.com>
2024-02-02 17:27:37 +1100

    feat: Add breakpoints to carousel

--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,14 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import { Carousel } from "../../components/Carousel";
+import type { Program } from "../../../static/types";
 import styles from "../Layout.module.css";
 
 export const Home = () => {
   const navigation = (id: string) => console.log(id);
-  const data = useLoaderData();
+  const data = useLoaderData() as Program[];
+  const items = data.map((x) => ({ id: x.id, title: x.title, image: x.image }));
 
-  return (
+  return data ? (
     <div className={styles.container}>
-      <Carousel onClick={navigation} items={[]} />
+      <Carousel onClick={navigation} items={items} />
     </div>
-  );
+  ) : null;
 };
